1e3f29be71ea24323672569f620aaf89770e9a8c
Dohyun Lee <xxrcy32@gmail.com>
2019-10-23 20:51:05 +0900

    post read

---
+++ public/scripts/firebaseConfig.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC4jN6MCZ-GN6RXg6rtNwGmNEDhBCVpvXQ",
+    authDomain: "writing-web-e3771.firebaseapp.com",
+    databaseURL: "https://writing-web-e3771.firebaseio.com",
+    projectId: "writing-web-e3771",
+    storageBucket: "writing-web-e3771.appspot.com",
+    messagingSenderId: "184184590919",
+    appId: "1:184184590919:web:8466f91bd0b1944844d762"
+  };
